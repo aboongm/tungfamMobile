18277ccc41dd2a51238a85e1fefd381fd84057b4
aboongm <aboongm@yahoo.com>
2023-10-17 22:04:38 +0530

    Add fonts in assets folder

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+function App() {
+
+  return (
+    <SafeAreaView >
+      <View>
+        <Text>Start Page</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
