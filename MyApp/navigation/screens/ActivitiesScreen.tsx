cca7deb0cdd8f6887650307996bdb5592e48d0c4
aboongm <aboongm@yahoo.com>
2023-12-15 09:38:28 +0530

    Update Activities.tsx to use user role

--- MyApp/navigation/screens/ActivitiesScreen.tsx
+++ MyApp/navigation/screens/ActivitiesScreen.tsx
@@ -1,30 +1,94 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Icon from "react-native-vector-icons/FontAwesome"
-import { COLORS } from '../../constants'
-import PageContainer from '../../components/PageContainer'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { COLORS } from '../../constants';
+import PageContainer from '../../components/PageContainer';
 
-const ActivitiesScreen = () => {
+const ActivitiesScreen = ({ userRole }) => {
+  const renderActions = () => {
+    switch (userRole) {
+      case 'admin':
+        return (
+          <View>
+            {/* Action for admin */}
+            <Text>Approve Firm</Text>
+          </View>
+        );
+      case 'firmOwner':
+        return (
+          <View>
+            {/* Actions for firmOwner */}
+            <Text>Create/Update/Delete LoanBook</Text>
+            <Text>Create/Update/Delete LoanType</Text>
+            <Text>Create/Update/Delete PaymentSchedule</Text>
+            {/* Add other actions for firmOwner */}
+          </View>
+        );
+      case 'loanOfficer':
+        return (
+          <View>
+            {/* Actions for loanOfficer */}
+            <Text>Update LoanBook</Text>
+            <Text>Update PaymentSchedule</Text>
+          </View>
+        );
+      case 'borrower':
+        return (
+          <View>
+            {/* Actions for borrower */}
+            <Text>View LoanBook</Text>
+            <Text>View LoanType</Text>
+            <Text>Read PaymentSchedule</Text>
+          </View>
+        );
+      case 'user':
+        return (
+          <View>
+            {/* Actions for user */}
+            <Text>Apply for a loan</Text>
+            <Text>Apply to open a firm</Text>
+          </View>
+        );
+      default:
+        return (
+          <View>
+            {/* Default actions for unknown role */}
+            <Text>No actions available for this role</Text>
+          </View>
+        );
+    }
+  };
+
   return (
     <PageContainer style={styles.container}>
-    <View>
-      <Text style={{fontFamily: "system", fontSize: 22}}>Activities Screen</Text>
-      <Text style={{fontFamily: "sans-serif", fontSize: 22}}>Activities Screen</Text>
-      <Text style={{fontFamily: "serif", fontSize: 22}}>Activities Screen</Text>
-      <Text style={{fontFamily: "monospace", fontSize: 22}}>Activities Screen</Text>
-      <Text style={{fontFamily: "roboto", fontWeight: 800, fontSize: 22}}>Activities Screen</Text>
-    </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Activities Screen</Text>
+        {renderActions()}
+      </View>
     </PageContainer>
-  )
-}
+  );
+};
 
-export default ActivitiesScreen
+// Map state to props to access userRole from Redux store
+const mapStateToProps = (state) => ({
+  userRole: state.auth.userData.role, // Assuming 'role' is the key for user role in your userData object
+});
+
+export default connect(mapStateToProps)(ActivitiesScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     borderWidth: 1,
     borderColor: COLORS.tungfamGrey,
-    margin: 4
+    margin: 4,
   },
-})
+  content: {
+    padding: 20, // Adjust as needed
+  },
+  title: {
+    fontFamily: 'roboto',
+    fontWeight: '800',
+    fontSize: 22,
+  },
+});
