aeea76859d7032f3dc0913fd67403868e631e9ce
aboongm <aboongm@yahoo.com>
2023-12-20 19:01:12 +0530

    update

--- MyApp/redux/reducers/employeesSlice.ts
+++ MyApp/redux/reducers/employeesSlice.ts
@@ -1,4 +1,7 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_URL } from '@env';
 
 interface Employee {
   employee_firm_id: string;
@@ -30,6 +33,57 @@ const initialState: EmployeesState = {
   // Initialize other state properties if needed
 };
 
+export const fetchEmployees = createAsyncThunk(
+  'employees/fetchEmployees',
+  async (firmDetails, { rejectWithValue }) => {
+      try {
+          const token = await AsyncStorage.getItem('token');
+          if (!token) {
+              throw new Error('Token not found');
+          }
+
+          const headers = {
+              Authorization: `${token}`,
+          };
+
+          const response = await axios.get(`${API_URL}/employeefirm/`, { headers });
+
+          if (response.status === 200) {
+              const employeesData = response.data;
+              const updatedEmployeesData = [];
+
+              for (const employee of employeesData) {
+                  if (firmDetails && employee.firm_id === firmDetails.firm_id) {
+                      const userDetails = await fetchUserDetails(employee.user_id, headers);
+                      if (userDetails) {
+                          updatedEmployeesData.push({
+                              ...employee,
+                              userDetails: userDetails ? userDetails : {}
+                          });
+                      }
+                  }
+              }
+
+              return updatedEmployeesData;
+          }
+      } catch (error) {
+          return rejectWithValue(error.message);
+      }
+  }
+);
+
+const fetchUserDetails = async (userId, headers) => {
+  try {
+      const userResponse = await axios.get(`${API_URL}/users/${userId}`, { headers });
+      if (userResponse.status === 200) {
+          return userResponse.data; // Return user details
+      }
+  } catch (error) {
+      console.error(`Error fetching user details for user ID ${userId}:`, error);
+      return null;
+  }
+};
+
 const employeesSlice = createSlice({
   name: 'employees',
   initialState,
@@ -46,14 +100,19 @@ const employeesSlice = createSlice({
       );
     },
     // Add other employee-related actions here
+    
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchEmployees.fulfilled, (state, action) => {
+      state.employees = action.payload;
+    });
+  }
 });
 
 export const {
   setEmployees,
   addEmployee,
   removeEmployee,
-  // Export other actions if needed
 } = employeesSlice.actions;
 
 export default employeesSlice.reducer;
