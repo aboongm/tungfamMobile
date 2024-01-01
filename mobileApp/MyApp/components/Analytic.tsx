d71f00a8cb19f0d91226c42a8a9ddcd030384e67
aboongm <aboongm@yahoo.com>
2024-01-01 14:12:05 +0530

    Update FirmValue, etc

--- mobileApp/MyApp/components/Analytic.tsx
+++ mobileApp/MyApp/components/Analytic.tsx
@@ -12,12 +12,13 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
     const navigation = useNavigation();
 
     const [showDetails, setShowDetails] = useState(false);
-    const [displayOption, setDisplayOption] = useState('Investors');
+    const [displayOption, setDisplayOption] = useState('Financials');
 
     // Financial data state
     const [totalInvestments, setTotalInvestments] = useState([]);
     const [totalOutstandingAmount, setTotalOutstandingAmount] = useState(0);
     const [cashBalance, setCashBalance] = useState(0);
+    const [showTotalInvestments, setShowTotalInvestments] = useState(true);
 
     useEffect(() => {
         // Fetch financial data
@@ -26,6 +27,10 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
 
     const toggleAnalytics = () => {
         setShowDetails(!showDetails);
+    };
+
+    const toggleTotalInvestment = () => {
+        setShowTotalInvestments(!showTotalInvestments);
     };
 
     const toggleDisplayOption = (option) => {
@@ -43,8 +48,6 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
             // Fetch cash balance (Replace with actual API call)
             const cash = await getCashBalance();
 
-            console.log(investorsData, outstandingAmount, cash);
-
             // Update state with fetched data
             if (investorsData && outstandingAmount && cash) {
 
@@ -58,18 +61,77 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
     };
 
     const getInvestorsTotalInvestments = () => {
-        return [200000]
-    }
+        return [{
+            name: 'Mayengbam Ranjit Luwang',
+            investments: [
+                {
+                    date: '10Oct23',
+                    amount: '27000',
+                },
+                {
+                    date: '20Nov23',
+                    amount: '50000',
+                },
+            ],
+        },
+        {
+            name: 'Mayengbam Ranjita Chanu',
+            investments: [
+                {
+                    date: '10Oct23',
+                    amount: '27000',
+                },
+                {
+                    date: '20Nov23',
+                    amount: '50000',
+                },
+            ],
+        }]
+    };
 
-    const getTotalOutstandingAmount = () => {
-        return 300000
+
+    const getTotalOutstandingAmount = async () => {
+        try {
+            const token = await AsyncStorage.getItem("token");
+            const headers = { Authorization: `${token}`}
+
+            
+            const response = await axios.get(`${API_URL}/loans`, { headers })
+            const filteredLoans = response.data.filter(
+                (loan) => loan.lender_firm_id === firmDetails.firm_id && loan.status === "approved"
+            );
+                
+            // console.log("filteredLoans: ", filteredLoans);
+            // const paymentResponse = await axios.get(`${API_URL}/loans/9/paymentschedules`, { headers })
+            // console.log("Payments: ", paymentResponse);
+            
+            const totalOutstanding = filteredLoans.reduce(
+                (total, loan) => total + loan.amount,
+                0
+            );
+                console.log(totalOutstanding);
+                
+            return totalOutstanding;
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     const getCashBalance = () => {
         return 10000
     }
 
-    const firmValue = totalInvestments.reduce((acc, investment) => acc + investment.amount, 0) + totalOutstandingAmount + cashBalance;
+    const calculateTotalInvestments = () => {
+        let total = 0;
+        totalInvestments.forEach((investor) => {
+            investor.investments.forEach((investment) => {
+                total += parseInt(investment.amount);
+            });
+        });
+        return total;
+    };
+
+    const firmValue = totalOutstandingAmount + cashBalance;
 
     return (
         <>
@@ -79,24 +141,6 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
             {showDetails && (
                 <>
                     <View style={styles.buttonContainer}>
-                    <Pressable
-                            style={[
-                                styles.buttonOptions,
-                                displayOption === 'Investors' && {
-                                    backgroundColor: COLORS.TungfamBgColor,
-                                },
-                            ]}
-                            onPress={() => toggleDisplayOption('Investors')}
-                        >
-                            <Text
-                                style={[
-                                    styles.buttonText,
-                                    displayOption === 'Investors' && { color: COLORS.white },
-                                ]}
-                            >
-                                Investors
-                            </Text>
-                        </Pressable>
                         <Pressable
                             style={[
                                 styles.buttonOptions,
@@ -113,6 +157,24 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
                                 ]}
                             >
                                 Financials
+                            </Text>
+                        </Pressable>
+                        <Pressable
+                            style={[
+                                styles.buttonOptions,
+                                displayOption === 'Expenses' && {
+                                    backgroundColor: COLORS.TungfamBgColor,
+                                },
+                            ]}
+                            onPress={() => toggleDisplayOption('Expenses')}
+                        >
+                            <Text
+                                style={[
+                                    styles.buttonText,
+                                    displayOption === 'Expenses' && { color: COLORS.white },
+                                ]}
+                            >
+                                Expenses
                             </Text>
                         </Pressable>
                         <Pressable
@@ -135,36 +197,58 @@ const Analytic = ({ firmDetails, userRole, userId }) => {
                         </Pressable>
                     </View>
 
-                    {displayOption === 'Investors' && (
-                        <View style={styles.investorsContainer}>
-                            <Text style={styles.sectionTitle}>Investors Section Skeleton</Text>
+                    {displayOption === 'Financials' && (
+                        <View style={styles.financialsContainer}>
+                            <Text style={styles.financialsTitle}>Financials</Text>
+                            <View style={styles.financialsData}>
+
+                                <View style={styles.itemContainer}>
+                                    <Text style={styles.item}>Total Outstanding Amount:</Text>
+                                    <Text style={styles.item}>Rs {totalOutstandingAmount}</Text>
+                                </View>
+
+                                <TouchableOpacity onPress={toggleTotalInvestment}>
+                                    <View style={styles.toggelInvestmentItemContainer}>
+                                        <Text style={styles.item}>Total Investments:</Text>
+                                        <Text style={styles.item}>Rs {calculateTotalInvestments()}</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                
+                                {showTotalInvestments && (
+                                    <View style={{ marginBottom: 10 }}>
+                                        {totalInvestments.map((investor, index) => (
+                                            <View key={index} style={styles.investmentsContainer}>
+                                                <Text style={styles.item}>{index + 1}. {investor.name}</Text>
+                                                {investor.investments.map((investment, i) => (
+                                                    <View key={i} style={styles.investmentItemContainer}>
+                                                        <Text style={styles.investmentItem}>{investment.date}</Text>
+                                                        <Text style={styles.investmentItem}>Rs {investment.amount}</Text>
+                                                    </View>
+                                                ))}
+                                            </View>
+                                        ))}
+                                    </View>
+                                )}
+                               
+                                <View style={styles.itemContainer}>
+                                    <Text style={styles.item}>Cash Balance:</Text>
+                                    <Text style={styles.item}>Rs {cashBalance}</Text>
+                                </View>
+                                <View style={[styles.itemContainer, { paddingVertical: 1}]}>
+                                    <Text style={[styles.item, { fontWeight: '500', fontSize: 20 }]}>FIRM VALUE:</Text>
+                                    <Text style={[styles.item, { fontWeight: '500', fontSize: 20, color: COLORS.TungfamBgColor }]}>Rs {firmValue}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    )}
+
+                    {displayOption === 'Expenses' && (
+                        <View style={styles.expensesContainer}>
+                            <Text style={styles.sectionTitle}>expensesContainer Section Skeleton</Text>
                             {/* Add skeleton structure for Investors section */}
                         </View>
                     )}
 
-                    {/* Render Financials section */}
-                    {displayOption === 'Financials' && (
-                        <View style={styles.financialsContainer}>
-                        <Text style={styles.financialsTitle}>Financials</Text>
-                        <View style={styles.financialsData}>
-                            <Text>Total Investments from Investors:</Text>
-                            {totalInvestments.map((investment, index) => (
-                                <View key={index}>
-                                    <Text>Investor {index + 1}</Text>
-                                    <Text>Investment Amount: {investment}</Text>
-                                </View>
-                            ))}
-
-                            <Text>Total Outstanding Amount from LoanBook: {totalOutstandingAmount}</Text>
-
-                            <Text>Cash Balance: {cashBalance}</Text>
-
-                            <Text>Firm Value: {firmValue}</Text>
-                        </View>
-                    </View>
-                    )}
-
-                    {/* Render TrendChart section */}
                     {displayOption === 'TrendChart' && (
                         <View style={styles.trendChartContainer}>
                             <Text style={styles.sectionTitle}>TrendChart Section Skeleton</Text>
@@ -202,7 +286,6 @@ const styles = StyleSheet.create({
     },
     buttonOptions: {
         flex: 1,
-        marginHorizontal: 1,
         paddingVertical: 10,
         alignItems: 'center',
         borderRadius: 5,
@@ -214,7 +297,6 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
     financialsContainer: {
-        marginVertical: 10,
         padding: 10,
         backgroundColor: 'rgba(255, 255, 255, 0.6)',
         borderWidth: 1,
@@ -228,6 +310,54 @@ const styles = StyleSheet.create({
     },
     financialsData: {
         // Style financial data as needed
+    },
+    itemContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderWidth: 1,
+        borderColor: COLORS.tungfamGrey,
+        borderRadius: 6,
+        paddingVertical: 4
+    },
+    toggelInvestmentItemContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderWidth: 1,
+        borderColor: COLORS.tungfamGrey,
+        borderRadius: 50,
+        paddingVertical: 4,
+        marginVertical: 4,
+    },
+    investmentsContainer: {
+        padding: 4,
+        // backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        borderWidth: 1,
+        borderColor: COLORS.tungfamGrey,
+        borderRadius: 6,
+    },
+    investmentItemContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    investmentItem: {
+        paddingHorizontal: 10,
+        paddingVertical: 2
+    },
+    item: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        paddingVertical: 6,
+        paddingHorizontal: 14,
+    },
+    expensesContainer: {
+        marginVertical: 10,
+        padding: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.6)',
+        borderWidth: 1,
+        borderColor: COLORS.tungfamGrey,
+        borderRadius: 6,
     },
     investorsContainer: {
         marginVertical: 10,
